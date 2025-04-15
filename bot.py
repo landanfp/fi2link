5248ee0843b9f048ae234d9dab7489fc10ed0cdd
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-15 15:24:59 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from helper.jsondb import save_file_info
 
-LOG_CHANNEL = -1001234567890
+LOG_CHANNEL = -1001792962793
 WEB_DOMAIN = "https://obnoxious-nichole-farsa-1880b732.koyeb.app/"
 
 def setup_handlers(app):
